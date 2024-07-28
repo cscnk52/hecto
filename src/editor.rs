997be29a38ddea81058e1dcfc92a2f57c923b860
cnk <cscnk52@outlook.com>
2024-07-28 20:22:50 +0800

    feat: Assigment 2  Improve Tildes

--- src/editor.rs
+++ src/editor.rs
@@ -1,6 +1,7 @@
-use crossterm::event::{read,Event,Event::Key,KeyCode::Char,KeyEvent,KeyModifiers};
+use crossterm::event::{read, Event, Event::Key, KeyCode::Char, KeyEvent, KeyModifiers};
 mod terminal;
-use terminal::Terminal;
+use std::io::Error;
+use terminal::{Position, Size, Terminal};
 
 pub struct Editor {
     should_quit: bool,
@@ -16,7 +17,7 @@ impl Editor {
         Terminal::terminate().unwrap();
         result.unwrap();
     }
-    fn repl(&mut self) -> Result<(), std::io::Error> {
+    fn repl(&mut self) -> Result<(), Error> {
         loop {
             self.refresh_screen()?;
             if self.should_quit {
@@ -41,21 +42,25 @@ impl Editor {
         }
     }
     fn refresh_screen(&self) -> Result<(), std::io::Error> {
+        Terminal::hide_cursor()?;
         if self.should_quit {
             let _ = Terminal::clean_screen();
-            print!("GoodBye.\r\n");
-        }else {
+            Terminal::print("GoodBye.\r\n")?;
+        } else {
             Self::draw_rows()?;
-            let _ = Terminal::move_cursor_to(0, 0);
+            let _ = Terminal::move_cursor_to(Position { x: 0, y: 0 });
+            Terminal::show_cursor()?;
+            Terminal::execute()?;
         }
         Ok(())
     }
-    fn draw_rows() -> Result<(),std::io::Error> {
-        let height = Terminal::size()?.1;
+    fn draw_rows() -> Result<(), std::io::Error> {
+        let Size { height, .. } = Terminal::size()?;
         for current_row in 0..height {
-            print!("~");
-            if  current_row + 1 < height {
-                print!("\r\n");
+            Terminal::clean_line()?;
+            Terminal::print("~")?;
+            if current_row + 1 < height {
+                Terminal::print("\r\n")?;
             }
         }
         Ok(())
