17f4d5e9025f23aa34e98ec9cf93da1ec27f9cfd
cnk <cscnk52@outlook.com>
2024-08-19 09:37:51 +0800

    feat(Assignment): finish Assignment 17 Complex Deletion

--- src/editor/view.rs
+++ src/editor/view.rs
@@ -46,8 +46,10 @@ impl View {
 
     // region: Text editing
     fn backspace(&mut self) {
-        self.move_left();
-        self.delete();
+        if self.text_location.line_index != 0 || self.text_location.grapheme_index != 0 {
+            self.move_text_location(&Direction::Left);
+            self.delete();
+        }
     }
     fn delete(&mut self) {
         self.buffer.delete(self.text_location);
@@ -68,7 +70,7 @@ impl View {
         let grapheme_delta = new_len.saturating_sub(old_len);
         if grapheme_delta > 0 {
             //move right for an added grapheme (should be the regular case)
-            self.move_right();
+            self.move_text_location(&Direction::Right);
         }
         self.need_redraw = true;
     }
