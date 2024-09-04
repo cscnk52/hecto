c7e2b5c28d641cfd7bba2af361e232fd03b68084
cnk <cscnk52@outlook.com>
2024-09-04 19:23:20 +0800

    feat(Assignment): finish Assignment 24 Better Communication to the User

--- src/editor.rs
+++ src/editor.rs
@@ -1,3 +1,4 @@
+use command::Command;
 use crossterm::event::{read, Event, KeyEvent, KeyEventKind};
 use uicomponent::UIComponent;
 use view::View;
@@ -9,18 +10,25 @@ use std::{
     panic::{set_hook, take_hook},
 };
 use terminal::Terminal;
-mod editorcommand;
-use editorcommand::EditCommand;
 mod statusbar;
 use statusbar::StatusBar;
+mod command;
 mod documentstatus;
 mod fileinfo;
 mod messagebar;
 mod uicomponent;
-use self::{messagebar::MessageBar, terminal::Size};
 use documentstatus::DocumentStatus;
 pub const NAME: &str = env!("CARGO_PKG_NAME");
 pub const VERSION: &str = env!("CARGO_PKG_VERSION");
+use self::{
+    command::{
+        Command::{Edit, Move, System},
+        System::{Quit, Resize, Save},
+    },
+    messagebar::MessageBar,
+    terminal::Size,
+};
+const QUIT_TIMES: u8 = 3;
 
 #[derive(Default)]
 pub struct Editor {
@@ -30,6 +38,7 @@ pub struct Editor {
     title: String,
     message_bar: MessageBar,
     terminal_size: Size,
+    quit_times: u8,
 }
 
 impl Editor {
@@ -43,13 +52,17 @@ impl Editor {
         let mut editor = Self::default();
         let size = Terminal::size().unwrap_or_default();
         editor.resize(size);
-        let args: Vec<String> = env::args().collect();
-        if let Some(file_name) = args.get(1) {
-            editor.view.load(file_name);
-        }
         editor
             .message_bar
-            .update_message("HELP: Ctrl-S = save | Ctrl-Q = quit".to_string());
+            .update_message("HELP Ctrl-S = save | Ctrl-Q = quit");
+        let args: Vec<String> = env::args().collect();
+        if let Some(file_name) = args.get(1) {
+            if editor.view.load(file_name).is_err() {
+                editor
+                    .message_bar
+                    .update_message(&format!("ERR: Could not open file: {file_name}"));
+            }
+        }
         editor.refresh_status();
         Ok(editor)
     }
@@ -106,15 +119,49 @@ impl Editor {
             _ => false,
         };
         if should_process {
-            if let Ok(command) = EditCommand::try_from(event) {
-                if matches!(command, EditCommand::Quit) {
-                    self.should_quit = true;
-                } else if let EditCommand::Resize(size) = command {
-                    self.resize(size);
-                } else {
-                    self.view.handle_command(command);
-                }
+            if let Ok(command) = Command::try_from(event) {
+                self.process_command(command);
             }
+        }
+    }
+    fn process_command(&mut self, command: Command) {
+        match command {
+            System(Quit) => self.handle_quit(),
+            System(Resize(size)) => self.resize(size),
+            _ => self.reset_quit_times(),
+        }
+
+        match command {
+            System(Quit | Resize(_)) => {}
+            System(Save) => self.handle_save(),
+            Edit(edit_command) => self.view.handle_edit_command(edit_command),
+            Move(move_command) => self.view.handle_move_command(move_command),
+        }
+    }
+    fn handle_save(&mut self) {
+        if self.view.save().is_ok() {
+            self.message_bar.update_message("File saved successfully.");
+        } else {
+            self.message_bar.update_message("Error writing file!");
+        }
+    }
+    // clippy::arithmetic_side_effects: quit_times is guaranteed to be between 0 and QUIT_TIMES
+    #[allow(clippy::arithmetic_side_effects)]
+    fn handle_quit(&mut self) {
+        if !self.view.get_status().is_modified || self.quit_times + 1 == QUIT_TIMES {
+            self.should_quit = true;
+        } else if self.view.get_status().is_modified {
+            self.message_bar.update_message(&format!(
+                "WARNING! File has unsaved changes. Press Ctrl-Q {} more times to quit.",
+                QUIT_TIMES - self.quit_times - 1
+            ));
+            self.quit_times += 1;
+        }
+    }
+    fn reset_quit_times(&mut self) {
+        if self.quit_times > 0 {
+            self.quit_times = 0;
+            self.message_bar.update_message("");
         }
     }
     fn refresh_screen(&mut self) {
